cc94269cfd47d1f1a46f800760b4e0a4fba022f1
Cristabelle Tumola <cristabelle.tumola@gmail.com>
2018-07-06 16:46:04 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,7 @@
-def display_board(board)
-board = (" "," "," "," "," "," "," "," "," ")
-puts "#{board[1]} |#{board[2]} |#{board[3]}"
-puts "-----------"
-puts "#{board[4]} |#{board[5]} |#{board[6]}"
-puts "-----------"
-puts "#{board[7]} |#{board[8} |#{board[9]}"
+def display_board
+  puts "  "  " |  "  " |  "  " "
+  puts "-----------"
+  puts "  "  " |  "  " |  "  " "
+  puts "-----------"
+  puts "  "  " |  "  " |  "  " "
 end
